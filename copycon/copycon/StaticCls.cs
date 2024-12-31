a792d502b37bc4b3dedfc7b414b2f6338bfc28f7
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-12-31 17:19:43 +0545

    first commit

---
+++ copycon/copycon/StaticCls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copycon
+{
+    class StaticCls
+
+    {
+       static StaticCls()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+    }
+}
